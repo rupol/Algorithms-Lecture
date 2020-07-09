cd4a039593217115915eeb49ffa0e01654b8be0b
Ruth Poliakon <r.poliakon@gmail.com>
2020-07-08 18:31:08 -0600

    code challenge

---
+++ src/challenges/7-8-20.py
@@ -0,0 +1,19 @@
+# Add up and print the sum of the all of the minimum elements of each inner array:
+# [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]]
+# The expected output is given by:
+# 4 + -1 + 9 + -56 + 201 + 18 = 175
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+def sum_mins(arr):
+    smallest = 0
+    for inner_array in arr:
+        # find min for each inner array
+        smallest += min(inner_array)
+    # sum all the smallests from the inner array and return
+    return smallest
+
+
+my_list = [[8, 4], [90, -1, 3], [9, 62], [-7, -1, -56, -6], [201], [76, 18]]
+
+print(sum_mins(my_list))
